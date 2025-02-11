9312a23c747bac724091d60cb5c7128d7ecffdc8
Nick Reeder <nreeder@phase2technology.com>
2025-02-11 13:13:33 -0800

    add location response and request dto

--- Sources/HoopFinderSharedDTO/SharedDTO.swift
+++ Sources/HoopFinderSharedDTO/SharedDTO.swift
@@ -29,4 +29,47 @@ public struct RegisterResponseDTO: Codable {
         self.error = error
         self.reason = reason
     }
+    
+    public struct LocationResponseDTO: Codable {
+        public let id: UUID
+        public let name: String
+        public let latitude: Double
+        public let longitude: Double
+        public let address: String
+        public let isOutside: Bool
+        public let hasLights: Bool
+        public let needMembership: Bool
+        
+        public init(id: UUID, name: String, latitude: Double, longitude: Double, address: String, isOutside: Bool, hasLights: Bool, needMembership: Bool) {
+            self.id = id
+            self.name = name
+            self.latitude = latitude
+            self.longitude = longitude
+            self.address = address
+            self.isOutside = isOutside
+            self.hasLights = hasLights
+            self.needMembership = needMembership
+        }
+        
+    }
+    public struct LocationRequestDTO: Codable {
+        
+        public let name: String
+        public let latitude: Double
+        public let longitude: Double
+        public let address: String
+        public let isOutside: Bool
+        public let hasLights: Bool
+        public let needMembership: Bool
+        
+        public init(name: String, latitude: Double, longitude: Double, address: String, isOutside: Bool, hasLights: Bool, needMembership: Bool) {
+            self.name = name
+            self.latitude = latitude
+            self.longitude = longitude
+            self.address = address
+            self.isOutside = isOutside
+            self.hasLights = hasLights
+            self.needMembership = needMembership
+        }
+    }
 }
